ec7b2cfcc354f165a7ea7b0ce37787f478a3e969
Rainer Martínez Fraga <rmartinezfraga@gmail.com>
2024-04-16 14:19:42 -0400

    make hello world

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,33 @@
+import {
+	AbsoluteFill,
+	interpolate,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+
 export const MyComposition = () => {
-	return null;
+	const frame = useCurrentFrame();
+	const { fps } = useVideoConfig();
+	const opacity = interpolate(frame, [0, 60], [0, 1], {
+		extrapolateRight: 'clamp',
+	});
+	const scale = spring({
+    fps,
+    frame,
+  });
+
+	return (
+		<AbsoluteFill
+			style={{
+				fontFamily: 'system-ui',
+				justifyContent: 'center',
+				alignItems: 'center',
+				fontSize: 100,
+				backgroundColor: 'white',
+			}}
+		>
+			<div style={{opacity, transform: `scale(${scale})`}}>Hello World</div>
+		</AbsoluteFill>
+	);
 };
